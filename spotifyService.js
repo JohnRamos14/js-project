1e49e5da5a178d6eeb0001a1bcc655c80b155c42
JohnRamos14 <Ramos.Johnmba@gmail.com>
2022-12-11 14:29:45 -0800

    first commit

---
+++ spotifyService.js
@@ -0,0 +1,1 @@
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=0f4b35304e414b268a8f9aa7b14cd8fd&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
